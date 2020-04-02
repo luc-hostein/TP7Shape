5982da238b2a7f1f4165cc594c2a0bfe25a1f1e8
luc-hostein <luc.host44@laposte.net>
2020-04-02 11:51:18 +0200

    work

--- exo3.php
+++ exo3.php
@@ -1,5 +1,38 @@
 <?php
 
+trait traitUn{
+    function small($text){
+        echo "<small>$text</small>";
+    }
+    function big($text){
+        echo "<h4>$text</h4>";
+    }
+}
+
+trait traitDeux{
+    function small($text){
+        echo "<i>$text</i>";
+    }
+    function big($text){
+        echo "<h2>$text</h2>";
+    }
+}
+
+class texte{
+    use traitUn,traitDeux{
+        traitDeux::small insteadof traitUn;
+        traitUn::big insteadof traitDeux;
+        traitDeux::big as gros;
+        traitUn::small as petit;
+    }
+}
+
+
+$text = new texte();
+$text->small("deux small");
+$text->big("un big");
+$text->gros("deux big");
+$text->petit("un small");
 
 
 ?>
